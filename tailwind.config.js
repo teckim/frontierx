908d1aa70bcb47b0bce419d2420b6743955a6a9d
BHDHakim <hakim.bhd@gmail.com>
2022-08-31 19:38:07 +0300

    Add base setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +7,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["'Manrope'", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: 'rgba(252, 54, 76, 1)',
+        dark: 'rgba(13, 13, 13, 1)',
+      }
+    },
   },
   plugins: [],
 }
