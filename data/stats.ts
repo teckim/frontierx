abeabc88ed573e670112cee1530ef7a789ded897
BHDHakim <hakim.bhd@gmail.com>
2022-09-08 22:08:01 +0300

    Add stats data

---
+++ data/stats.ts
@@ -0,0 +1,66 @@
+import { PropTypes } from '@/components/StatsCard'
+
+const STATS: PropTypes[] = [
+  {
+    name: 'Accessories & Ears',
+    label: 'Lorem ipsum',
+    percentage: 9,
+  },
+  {
+    name: 'Arms',
+    label: 'Lorem ipsum',
+    percentage: 15,
+  },
+  {
+    name: 'Background',
+    label: 'Lorem ipsum',
+    percentage: 2,
+  },
+  {
+    name: 'Eye',
+    label: 'Lorem ipsum',
+    percentage: 32,
+  },
+  {
+    name: 'Glasses',
+    label: 'Lorem ipsum',
+    percentage: 3,
+  },
+  {
+    name: 'Hair & Hats',
+    label: 'Lorem ipsum',
+    percentage: 2,
+  },
+  {
+    name: 'Head',
+    label: 'Lorem ipsum',
+    percentage: 92,
+  },
+  {
+    name: 'Legs & Clothes',
+    label: 'Lorem ipsum',
+    percentage: 9,
+  },
+  {
+    name: 'Mouth',
+    label: 'Lorem ipsum',
+    percentage: 46,
+  },
+  {
+    name: 'Glasses',
+    label: 'Lorem ipsum',
+    percentage: 43,
+  },
+  {
+    name: 'Eye',
+    label: 'Lorem ipsum',
+    percentage: 3,
+  },
+  {
+    name: 'Accessories & Ears',
+    label: 'Lorem ipsum',
+    percentage: 9,
+  },
+]
+
+export default STATS
