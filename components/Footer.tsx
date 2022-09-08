0909189cd5ebd258542dfceeedd785ecafddb18a
BHDHakim <hakim.bhd@gmail.com>
2022-09-08 22:08:37 +0300

    Add Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import LogoImage from '@/assets/images/logo-dark.svg'
+
+const Footer = () => {
+  return (
+    <div className="bg-dark">
+      <div className="container mx-auto flex justify-center py-20">
+        <LogoImage />
+      </div>
+    </div>
+  )
+}
+
+export default Footer
