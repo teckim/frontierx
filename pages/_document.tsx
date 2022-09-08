c99f472ca7e3eb6d7ea004a05199c596514baa0e
BHDHakim <hakim.bhd@gmail.com>
2022-09-08 22:04:03 +0300

    Update main document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,12 +7,24 @@ class MainDocument extends Document {
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
-            rel="stylesheet" 
+            rel="stylesheet"
           />
         </Head>
-        <body>
-          <Main />
-          <NextScript />
+        <body
+          className="bg-no-repeat text-white"
+          style={{
+            background: 'linear-gradient(0deg, #0D0D0D, #0D0D0D), #141417',
+          }}
+        >
+          <div
+            className="min-h-screen bg-cover"
+            style={{
+              backgroundImage: 'url(/background.png)',
+            }}
+          >
+            <Main />
+            <NextScript />
+          </div>
         </body>
       </Html>
     )
